dc265c08edad6ccda306a12932255a18f758127c
cris <cris12gm@gmail.com>
2021-12-20 15:22:09 +0100

    new helper

--- sRNAtoolboxweb/helpers/urls.py
+++ sRNAtoolboxweb/helpers/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^ncbiparser', views.NCBI.as_view(), name="ncbi"),
     url(r'^ensemlparser', views.Ensembl.as_view(), name="ensembl"),
     url(r'^rnacentralparser', views.RNAcentral.as_view(), name="rnacentral"),
+    url(r'^fasubset', views.Fasubset.as_view(), name="fasubset"),
     url(r'^trnaparser', views.Trna.as_view(), name="trna"),
-
     url(r'^removedup', views.RemoveDup.as_view(), name='removedup'),
     url(r'^extract', views.Extract.as_view(), name="EXTRACT"),
     url(r'results', views.result, name='helper'),
