2e4e811e5145e64e2c6ec472d815fa9894a445d3
cris <cris12gm@gmail.com>
2021-12-23 14:01:11 +0100

    finish fasubset

--- sRNAtoolboxweb/helpers/views.py
+++ sRNAtoolboxweb/helpers/views.py
@@ -228,13 +228,15 @@ def run(request, tool):
             uploaded_file = str(file_to_update)
             FS.save(uploaded_file, file_to_update)
             ifile = os.path.join(FS.location, uploaded_file)
-
         elif request.POST["url"].replace(" ", "") != "":
-            url_input = request.POST["url"]
-            dest = os.path.join(FS.location, os.path.basename(url_input))
-            handler = urllib.URLopener()
-            handler.retrieve(url_input, dest)
-            ifile = dest
+            try:
+                url_input = request.POST["url"]
+                dest = os.path.join(FS.location, os.path.basename(url_input))
+                handler = urllib.URLopener()
+                handler.retrieve(url_input, dest)
+                ifile = dest
+            except:
+                return render(request, "error_page.html", {"errors": ["URL of file must be provided"]})
 
         else:
             return render(request, "error_page.html", {"errors": ["URL of file must be provided"]})
