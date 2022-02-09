34f9ddba27eced4939e98ab712de3281f3f69456
cris <cris12gm@gmail.com>
2022-02-09 23:49:35 +0100

    fixed sRNAcons forms

--- sRNAtoolboxweb/sRNAcons/forms.py
+++ sRNAtoolboxweb/sRNAcons/forms.py
@@ -29,7 +29,7 @@ class sRNAconsForm(forms.Form):
     )
     ifile = forms.FileField(label='Upload input file(Fasta file)',required=False)
     url = forms.URLField(label='URL/link',required=False)
-    mistmatches = forms.IntegerField(label="Number of mistmatches", required=True, initial=1)
+    mistmatches = forms.IntegerField(label="Number of mistmatches", required=True, initial=1,min_value=0)
     kingdom=forms.ChoiceField(label= 'Kingdom', choices=KINGDOMS, required=False)
 
 
